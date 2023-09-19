dd99ddd4482939b37972c2ba9c5aacd998a20f45
jaheyliger <32755639+jaheyliger@users.noreply.github.com>
2023-09-19 00:02:22 -0400

    added prisma and planetscale

---
+++ app/(setup)/page.tsx
@@ -0,0 +1,28 @@
+import { initialProfile } from "@/lib/initial-profile"
+import { db } from "@/lib/db"
+import {redirect} from 'next/navigation'
+
+const SetupPage = async () => {
+
+    const profile = await initialProfile()
+
+    //search server that belongs to user
+    const server = await db.server.findFirst({
+        where: {
+            members: {
+                some: {
+                    profileId: profile.id
+                }
+            }
+        }
+    })
+
+// if server fouund return that server
+    if(server) return redirect(`/servers/${server.id}`)
+
+  return (
+    <div>Create a Server</div>
+  )
+}
+
+export default SetupPage
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,11 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Open_Sans } from 'next/font/google'
-import {ClerkProvider} from '@clerk/nextjs'
+import './globals.css';
+import type { Metadata } from 'next';
+import { Open_Sans } from 'next/font/google';
+import { ClerkProvider } from '@clerk/nextjs';
+import { ThemeProvider } from '@/components/providers/theme-provider';
+import { cn } from '@/lib/utils';
 
-const font = Open_Sans({ subsets: ['latin'] })
+const font = Open_Sans({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
 	title: 'Discord Clone',
@@ -11,14 +13,23 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
-  children,
+	children
 }: {
-  children: React.ReactNode
+	children: React.ReactNode;
 }) {
 	return (
 		<ClerkProvider>
-			<html lang='en'>
-				<body className={font.className}>{children}</body>
+			<html lang='en' suppressHydrationWarning>
+				<body className={cn(font.className, 'bg-white dark:bg-[#313338]')}>
+					<ThemeProvider
+						attribute='class'
+						defaultTheme='dark'
+						enableSystem={true}
+						storageKey='discord-theme'
+					>
+						{children}
+					</ThemeProvider>
+				</body>
 			</html>
 		</ClerkProvider>
 	);
