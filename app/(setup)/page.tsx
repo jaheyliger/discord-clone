d1a8dfce5125073818bad65808a362f5a56e3279
jaheyliger <32755639+jaheyliger@users.noreply.github.com>
2023-09-20 00:57:27 -0400

    added modal with file upload

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,6 +1,7 @@
 import { initialProfile } from "@/lib/initial-profile"
 import { db } from "@/lib/db"
 import {redirect} from 'next/navigation'
+import InitialModal from '@/components/modals/initial-modal';
 
 const SetupPage = async () => {
 
@@ -20,9 +21,7 @@ const SetupPage = async () => {
 // if server fouund return that server
     if(server) return redirect(`/servers/${server.id}`)
 
-  return (
-    <div>Create a Server</div>
-  )
+  return <InitialModal />;
 }
 
 export default SetupPage
